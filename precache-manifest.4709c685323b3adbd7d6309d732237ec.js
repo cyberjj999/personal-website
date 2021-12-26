9cc00d6588b7833c5ef8eaf88fb49985a6f64622
cyberjj999 <lyejiajun99@gmail.com>
2021-12-26 21:08:51 +0800

    Updates

---
+++ precache-manifest.4709c685323b3adbd7d6309d732237ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a64ef8b680843baccc03dd9a5acaf17",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "a9b5e2a996514627ea7a",
+    "url": "/personal-website/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d3a386c9007370d66a6a",
+    "url": "/personal-website/static/css/main.53632c19.chunk.css"
+  },
+  {
+    "revision": "a9b5e2a996514627ea7a",
+    "url": "/personal-website/static/js/2.67b6c21e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/personal-website/static/js/2.67b6c21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a386c9007370d66a6a",
+    "url": "/personal-website/static/js/main.6ed83183.chunk.js"
+  },
+  {
+    "revision": "603c35080e07d27fa18a",
+    "url": "/personal-website/static/js/runtime-main.65c197f1.js"
+  }
+]);
